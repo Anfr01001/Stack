cb9159f493da83c190ac9667839d21e4d71385d0
Vilgot <vilu02001@utb.vaxjo.se>
2019-10-14 16:04:57 +0200

    Stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Stack stack = new Stack();
+
+            string s = "Hej jagg< hetaaa<<<erasdf<<< inget";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(s[i] == '<')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(s[i].ToString());
+                }
+
+            }
+
+            for (int i = 0; i < stack.Count; i++)
+            {
+                Console.Write(stack[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    class Stack
+    {
+        private string[] stack;
+        private int count = 0;
+
+
+
+
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public string this[int i]
+        {
+            get { return stack[i]; }
+            set { stack[i] = value; }
+        }
+
+        public Stack() {
+            stack = new string[10]; 
+        }
+
+        public Stack(int size)
+        {
+            stack = new string[size];
+        }
+
+        public void Push(string value)
+        {
+            if (stack.Length == count)
+                ReSize((int)(count * 2));
+            stack[count] = value;
+            count++;
+        }
+
+        public string Pop()
+        {
+            count--;
+            if (count == stack.Length / 2 && stack.Length > 100)
+                ReSize((int)(count * 0.3));
+
+            return stack[count];
+        }
+
+        private void ReSize(int size)
+        {
+            string[] temp = stack;
+
+            stack = new string[size];
+
+            for (int i = 0; i < count; i++)
+            {
+                stack[i] = temp[i];
+            }
+        }
+
+        public void Clear()
+        {
+            count = 0;
+            stack = new string[10];
+        }
+
+    }
+}
